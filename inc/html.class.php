976cc8453ca7113e8f3fff0e37dc53d831b21f26
Stanislas Kita <skita@teclib.com>
2016-08-26 11:12:25 +0200

    do not upload doc if fileupload div not exist

--- inc/html.class.php
+++ inc/html.class.php
@@ -3905,7 +3905,6 @@ class Html {
          relative_urls: false,
          remove_script_host: false,
          entity_encoding: 'raw',
-         paste_data_images: true,
          menubar: false,
          statusbar: false,
          skin: 'light',
